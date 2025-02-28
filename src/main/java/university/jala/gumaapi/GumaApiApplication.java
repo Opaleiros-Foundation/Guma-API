7855a304a9a561f962450ab7ffc683da05ffa888
MatheusVict <Matheus.Henrique0724@jala.universitY>
2025-02-27 22:28:42 -0300

    initial commit

---
+++ src/main/java/university/jala/gumaapi/GumaApiApplication.java
@@ -0,0 +1,13 @@
+package university.jala.gumaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GumaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GumaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/university/jala/gumaapi/GumaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package university.jala.gumaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GumaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
